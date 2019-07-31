7878f6d783141c767c41b2f4bd5944489d3b0f29
Janet <zaied7jannet@gmail.com>
2019-07-31 13:18:45 +0100

    good

--- app/src/main/java/Model/ActivityFormulaire.java
+++ app/src/main/java/Model/ActivityFormulaire.java
@@ -9,8 +9,15 @@ import android.widget.Button;
 
 import com.example.cdlapp.R;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ActivityFormulaire extends AppCompatActivity {
     Button btn ;
+    private JsonPlaceHolderApi jsonPlaceHolderApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,28 @@ public class ActivityFormulaire extends AppCompatActivity {
             public void onClick(View view) {
                 Intent i= new Intent(ActivityFormulaire.this,HomeActivity.class);
                 startActivity(i);
+            }
+        });
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("é")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        jsonPlaceHolderApi= retrofit.create(JsonPlaceHolderApi.class);
+        createUser();
+
+    }
+    private void createUser(){
+        User user = new User("Jannet","Zaied","hhh");
+        Call<User>call= jsonPlaceHolderApi.createUser(user);
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+
             }
         });
     }
