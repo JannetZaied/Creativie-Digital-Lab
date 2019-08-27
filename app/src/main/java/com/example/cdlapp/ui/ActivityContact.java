49dd57ec39b509ab61cc2160ac5c80e369beb544
Janet <zaied7jannet@gmail.com>
2019-08-27 15:16:36 +0100

    good

--- app/src/main/java/com/example/cdlapp/ui/ActivityContact.java
+++ app/src/main/java/com/example/cdlapp/ui/ActivityContact.java
@@ -3,6 +3,8 @@ package com.example.cdlapp.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 import com.example.cdlapp.R;
 
@@ -12,5 +14,14 @@ public class ActivityContact extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+        TextView fcb= findViewById(R.id.fb);
+        fcb.setMovementMethod(LinkMovementMethod.getInstance());
+        TextView lin= findViewById(R.id.in);
+        lin.setMovementMethod(LinkMovementMethod.getInstance());
+        TextView web= findViewById(R.id.site);
+        lin.setMovementMethod(LinkMovementMethod.getInstance());
+
+
+
     }
 }
